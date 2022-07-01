7c5efddc53a28cc58566ac89353d228e27cbebd8
harleymacedo <harley.ip@gmail.com>
2022-07-01 08:58:37 -0300

    Post de grupo adicionado

--- router/grupoRouter.js
+++ router/grupoRouter.js
@@ -15,6 +15,18 @@ GrupoRouter.get('/grupo/:id', async (req, res) => {
 });
 
 GrupoRouter.post('/grupo', async (req, res) => {
+    try {
+        console.log(req.body);
+        await Grupo.create({
+            nome: req.body.nome,
+            descricao: req.body.descricao,
+            meta: req.body.meta,
+            imagem: req.body.imagem,
+        });
+        res.json({mensagem: 'Grupo cadastrado'});
+    } catch (error) {
+        res.json({mensagem: 'Erro no cadastro'});
+    }
     await res.json({});
 });
 
