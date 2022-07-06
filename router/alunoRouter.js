07dd05c0205523f3a1da5d0531b874a790525391
harleymacedo <harley.ip@gmail.com>
2022-07-05 23:49:53 -0300

    Rotas de aluno melhoradas

--- router/alunoRouter.js
+++ router/alunoRouter.js
@@ -11,11 +11,25 @@ AlunoRouter.get('/aluno', async (req, res) => {
 });
 
 AlunoRouter.get('/aluno/:id', async (req, res) => {
-    await res.json({});
+    try {
+        const result = await Aluno.findOne({email: req.body.email});
+        res.json(result);
+    } catch (error) {
+        res.json({mensagem: 'Erro na busca de aluno'});
+    }
 });
 
 AlunoRouter.post('/aluno', async (req, res) => {
-    await res.json({});
+    try {
+        const result = await Aluno.create({
+            nome: req.body.nome,
+            email: req.body.email,
+            telefone: req.body.telefone
+        });
+        res.json({mensagem: 'Aluno cadastrado'});
+    } catch (error) {
+        res.json({mensagem: 'Erro no cadastro do aluno'});
+    }
 });
 
 AlunoRouter.put('/aluno', async (req, res) => {
